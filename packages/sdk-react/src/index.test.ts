49f911dadff72f4a64260b4b9c67e9450014082b
Nils Jacobsen <nils.jacobsen98@gmail.com>
2025-11-04 11:47:21 +0100

    feat(legit-sdk/react)

--- packages/sdk-react/src/index.test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from 'vitest';
-import { addition } from '.';
-
-test('adds 1 + 2 to equal 3', () => {
-  expect(addition(1, 2)).toBe(3);
-});
--- packages/sdk-react/src/index.ts
+++ packages/sdk-react/src/index.ts
@@ -1,3 +1,4 @@
-export const addition = (a: number, b: number) => {
-  return a + b;
-};
+export { LegitProvider, useLegitContext } from './LegitProvider';
+export type { LegitContextValue, LegitProviderProps } from './LegitProvider';
+export { useLegitFile } from './useLegitFile';
+export type { UseLegitFileReturn } from './useLegitFile';
