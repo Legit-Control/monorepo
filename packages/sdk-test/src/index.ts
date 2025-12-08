0e7dec3792f5077541c80b74f958b7b9a2c8df92
Nils Jacobsen <nils.jacobsen98@gmail.com>
2025-12-08 10:59:56 +0100

    fix monorepo build and tests
    - set assistant-ui app to older version because it would need bigger refactor

--- packages/sdk-test/src/index.ts
+++
@@ -1,8 +0,0 @@
-// Import browser polyfills first to set up globals like Buffer
-import './browser-polyfills.js';
-
-// Browser-specific exports
-export * from './adapter/browser-fs-access.js';
-
-// Core exports
-export * from './index-server.js';
